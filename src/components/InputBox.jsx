ca800931d9f37489770989580114ebbeddc533d9
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-28 15:28:09 +0500

    Added Input Field With All Properties and Tailwind Classes

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -9,3 +9,18 @@ function InputBox({
   currencyDisable = false,
   className = "",
 }) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
