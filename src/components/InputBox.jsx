ed30d7ff4755d4503a16c91477e6ab81957ecf9b
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-28 15:27:27 +0500

    Make a function for Component & Destruct All Data Comes from props

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,11 @@
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) {
