46cabab2e5065566cc0e6b25d438eabe39e0399d
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-30 12:04:45 +0500

    Added Use State Values

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
   return <></>;
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("pkr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 }
 
 export default App;
