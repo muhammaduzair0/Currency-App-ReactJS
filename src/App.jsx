e50137bb9d46c1a0b89146f6a2ecba9c2b6a9143
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-30 12:06:10 +0500

    integrate useCurrencyInfo hook to fetch exchange rates based on selected currency

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  return <></>;
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("pkr");
   const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
 }
 
 export default App;
