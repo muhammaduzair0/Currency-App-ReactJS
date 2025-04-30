efe59ef00066373f3b73a15a40f42c605e7f9c37
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-30 12:06:36 +0500

    add swap functionality to interchange 'from' and 'to' currencies with corresponding amounts

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,28 @@ function App() {
   const [convertedAmount, setConvertedAmount] = useState(0);
 
   const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
   const convert = () => {
     setConvertedAmount(amount * currencyInfo[to]);
   };
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
+              >
+                swap
+              </button>
+            </div>
 }
 
 export default App;
